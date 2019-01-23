f0c257413e70ef816cd893a86fc7f84873a72b71
jdbxu <baixue12@jd.com>
2019-01-23 17:20:24 +0800

    errorhandler

--- app/errorhandlers.py
+++ app/errorhandlers.py
@@ -1,28 +1,30 @@
+from werkzeug import exceptions
 from flask import Blueprint, jsonify
 
 bp = Blueprint('errors', __name__)
 
 
-@bp.errorhandler(400)
-def page_not_found(error):
+@bp.errorhandler(exceptions.BadRequest)
+def bad_request(error):
     return jsonify(code=100400, msg='Bad Request')
 
 
-@bp.errorhandler(401)
-def page_not_found(error):
+@bp.errorhandler(exceptions.Unauthorized)
+def unauthorized(error):
     return jsonify(code=100401, msg='Unauthorized')
 
 
-@bp.errorhandler(403)
-def page_not_found(error):
+@bp.errorhandler(exceptions.Forbidden)
+def forbidden(error):
     return jsonify(code=100403, msg='Forbidden')
 
 
-@bp.errorhandler(404)
+@bp.errorhandler(exceptions.NotFound)
 def page_not_found(error):
     return jsonify(code=100404, msg='Page Not Found')
 
 
-@bp.errorhandler(500)
-def page_not_found(error):
-    return jsonify(code=100500, msg='Server Error')
+@bp.errorhandler(exceptions.InternalServerError)
+def internal_server_error(error):
+    return jsonify(code=100500, msg='Internal Server Error')
+
